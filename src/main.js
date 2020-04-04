2cc988b3ddcf3735fad010c434bd11dab6422467
Lucas Johnson <lucas.johnson@mediamonks.com>
2020-04-04 17:13:49 -0400

    added font awesome and updated social media icons

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-// Import main css
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '~/assets/style/index.scss'
-
-// Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
 
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+config.autoAddCss = false;
+library.add(faGithub, faTwitter)
+
 export default function (Vue, { router, head, isClient }) {
-  
-  // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+  Vue.component('font-awesome', FontAwesomeIcon);
 }
